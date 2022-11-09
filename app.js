1adcfc5dbf32ad60967266607a7c571c08a6cf92
Mattrix01 <emjay.knight@gmail.com>
2022-11-09 14:00:49 +0000

    Gernating card backgrounds with grid id

--- app.js
+++ app.js
@@ -49,4 +49,21 @@ const cardArray = [
   },
 ];
 
-console.log(cardArray);
+// array shuffled randomly, if larger or smaller than 0.5 it will check each object if below or above.
+cardArray.sort(() => 0.5 - Math.random());
+
+const gridDisplay = document.querySelector("#grid");
+
+// for each item in my array, create an element.
+function createBoard() {
+  for (let i = 0; i < 10; i++) {
+    const card = document.createElement("img");
+    // grabbing card we just created
+    card.setAttribute("src", "images/blank.png");
+    // Making sure each card is unique, keeping track of each one adding an id.
+    card.setAttribute("data-id", i);
+    gridDisplay.appendChild(card);
+  }
+}
+
+createBoard();
