cd85867a6dcbe9c74b0892fdb0d7c39d9d907a89
Mattrix01 <emjay.knight@gmail.com>
2022-11-09 15:53:27 +0000

    flip card function and pushing to chosen card arr

--- app.js
+++ app.js
@@ -53,17 +53,29 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector("#grid");
+const cardsChosen = [];
 
 // for each item in my array, create an element.
 function createBoard() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < cardArray.length; i++) {
     const card = document.createElement("img");
     // grabbing card we just created
     card.setAttribute("src", "images/blank.png");
     // Making sure each card is unique, keeping track of each one adding an id.
     card.setAttribute("data-id", i);
+    // listening for clicks
+    card.addEventListener("click", flipCard);
     gridDisplay.appendChild(card);
   }
 }
 
 createBoard();
+
+function flipCard() {
+  // whatever element we clicked and get its attribute data.
+  // this keyword is allowing us to interact to whatever element we click
+  const cardId = this.getAttribute("data-id");
+  // push item into a new array
+  cardsChosen.push(cardArray[cardId].name);
+  this.setAttribute("src", cardArray[cardId].img);
+}
